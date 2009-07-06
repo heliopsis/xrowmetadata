94dce399f7d086e044211097d23194a22cc02400
emil.webber <emil.webber@e429c339-3610-4ef8-b1a0-6d7923608c28>
2009-07-06 15:11:47 +0000

    Added german translation

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>design/standard/class/datatype</name>
+    <message>
+        <source>Title</source>
+        <translation>Titel (Meta Title)</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Beschreibung (Meta Description)</translation>
+    </message>
+    <message>
+        <source>Keywords</source>
+        <translation>Suchbegriffe (Meta Keywords)</translation>
+    </message>
+    <message>
+        <source>Googlemap</source>
+        <translation>Googlemap</translation>
+    </message>
+    <message>
+        <source>Change frequence</source>
+        <translation>Änderungsfrequenz</translation>
+    </message>
+    <message>
+        <source>always</source>
+        <translation>immer</translation>
+    </message>
+    <message>
+        <source>hourly</source>
+        <translation>stündlich</translation>
+    </message>
+    <message>
+        <source>daily</source>
+        <translation>täglich</translation>
+    </message>
+    <message>
+        <source>weekly</source>
+        <translation>wöchentlich</translation>
+    </message>
+    <message>
+        <source>monthly</source>
+        <translation>monatlich</translation>
+    </message>
+    <message>
+        <source>yearly</source>
+        <translation>jährlich</translation>
+    </message>
+    <message>
+        <source>never</source>
+        <translation>nie</translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation>Priorität</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Title</source>
+        <translation>Titel (Meta Title)</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Beschreibung (Meta Description)</translation>
+    </message>
+    <message>
+        <source>Keywords</source>
+        <translation>Suchbegriffe (Meta Keywords)</translation>
+    </message>
+    <message>
+        <source>Googlemap</source>
+        <translation>Googlemap</translation>
+    </message>
+    <message>
+        <source>Google Change</source>
+        <translation>Google Änderungsrate</translation>
+    </message>
+    <message>
+        <source>Google Priority</source>
+        <translation>Google Priorität</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/classes/datatypes</name>
+    <message>
+        <source>Metadata</source>
+        <comment>Datatype name</comment>
+        <translation>Metadaten</translation>
+    </message>
+    <message>
+        <source>Input required.</source>
+        <translation>Eingabe erforderlich.</translation>
+    </message>
+    <message>
+        <source>Title required.</source>
+        <translation>Titel - Eingabe erforderlich.</translation>
+    </message>
+</context>
+</TS>
